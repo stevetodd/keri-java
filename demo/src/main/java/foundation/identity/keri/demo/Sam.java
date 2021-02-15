439b8eee6ae3f971978905ae7c4236683ac8991e
Steve Todd <blurryrunner@gmail.com>
2021-02-15 15:13:36 -0700

    Retry connect on Bob and Sam demos

--- demo/src/main/java/foundation/identity/keri/demo/Sam.java
+++ demo/src/main/java/foundation/identity/keri/demo/Sam.java
@@ -3,9 +3,11 @@ package foundation.identity.keri.demo;
 import foundation.identity.keri.controller.Controller;
 import foundation.identity.keri.eventstorage.inmemory.InMemoryEventStore;
 import foundation.identity.keri.keystorage.inmemory.InMemoryIdentifierKeyStore;
+import reactor.util.retry.Retry;
 
 import java.net.InetSocketAddress;
 import java.security.SecureRandom;
+import java.time.Duration;
 import java.util.List;
 
 public class Sam {
@@ -20,11 +22,28 @@ public class Sam {
     var controller = new Controller(eventStore, keyStore, secureRandom);
     var identifier = controller.newPrivateIdentifier();
 
-    identifier.rotate();
     identifier.seal(List.of());
+    identifier.rotate();
 
     new DirectModeNode(identifier, eventStore)
         .connect(new InetSocketAddress("localhost", 5621))
+        .retryWhen(Retry.fixedDelay(Long.MAX_VALUE, Duration.ofSeconds(1)))
+        .block()
+        .onDispose()
+        .block();
+
+
+    new DirectModeNode(identifier, eventStore)
+        .connect(new InetSocketAddress("localhost", 5621))
+        .retryWhen(Retry.fixedDelay(Long.MAX_VALUE, Duration.ofSeconds(1)))
+        .block()
+        .onDispose()
+        .block();
+
+
+    new DirectModeNode(identifier, eventStore)
+        .connect(new InetSocketAddress("localhost", 5621))
+        .retryWhen(Retry.fixedDelay(Long.MAX_VALUE, Duration.ofSeconds(1)))
         .block()
         .onDispose()
         .block();
